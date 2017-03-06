20a94ba9300cb18b629f23c662e25b192ae1f3c9
William <william.nolascob@gmail.com>
2017-03-06 00:28:30 -0500

    primer commit

---
+++ client/js/header.js
@@ -0,0 +1,13 @@
+var item_link = document.querySelectorAll('.item-link'),
+	movable_bar = document.querySelector('.movable-bar');
+
+
+function hoverItem(posLeft){
+	return function(){
+		movable_bar.style.left = (posLeft*130) + 'px';
+	};
+}
+
+for(var a=0; a<item_link.length; a++){
+	item_link[a].addEventListener('mouseover', hoverItem(a));
+}
