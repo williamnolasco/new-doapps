e06377233a9d58ed1270fbf23b7ff3e37abf148e
William <william.nolascob@gmail.com>
2017-04-13 23:15:35 -0500

    detail-project.html finished

---
+++ client/js/detail-project.js
@@ -0,0 +1,42 @@
+function animations(){
+	if ($(window).width() >= 1024){
+		//Contador
+		var anima_item_b7 = $('.we-develop-bottom');
+		anima_item_b7.waypoint(function(direction) {
+			if (direction === 'down') {
+				$('.timer1').countTo({
+					from: 0,
+					to: 30,
+					speed: 1500,
+					refreshInterval: 50,
+					onComplete: function(value) {
+					}
+				});
+				$('.timer2').countTo({
+					from: 0,
+					to: 50,
+					speed: 1500,
+					refreshInterval: 50,
+					onComplete: function(value) {
+					}
+				});
+				$('.timer3').countTo({
+					from: 0,
+					to: 80,
+					speed: 1500,
+					refreshInterval: 50,
+					onComplete: function(value) {
+					}
+				});
+			}else{
+			}
+			}, {
+			offset:'90%'
+		});
+	}
+}
+
+animations();
+$(window).resize(function(event) {
+	animations();
+});
