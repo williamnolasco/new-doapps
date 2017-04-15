8599b8c9b99368abb305d7f2ec8fdc49edb2893f
William <william.nolascob@gmail.com>
2017-04-15 16:28:04 -0500

    header responsive

--- client/js/header.js
+++ client/js/header.js
@@ -24,3 +24,25 @@ for(var a=0; a<item_link.length; a++){
 }
 
 movable_bar.style.opacity = '0';
+
+
+//Menu Responsive
+
+var list_items = document.querySelector('.list-items'),
+	open_menu_mobile = document.querySelector('.open-menu-mobile'),
+	link_logo = document.querySelector('.link-logo'),
+	contador = 0;
+
+open_menu_mobile.addEventListener('click', function(){
+	if( contador === 0 ){
+		list_items.classList.add('active');
+		open_menu_mobile.classList.add('active');
+		link_logo.classList.add('active');
+		contador = 1;
+	}else if( contador === 1 ){
+		list_items.classList.remove('active');
+		open_menu_mobile.classList.remove('active');
+		link_logo.classList.remove('active');
+		contador = 0;
+	}
+});
