55b639dca350620b5748accc98e6ee23cb40e502
William <william.nolascob@gmail.com>
2017-05-04 06:26:57 -0500

    formulario finished

--- client/js/footer.js
+++ client/js/footer.js
@@ -3,14 +3,12 @@ var input_container = document.querySelectorAll('.input-container'),
 	row_input_content = document.querySelectorAll('.row-input-content'),
 	form_input = document.querySelectorAll('.form-input');
 
-function livelyBar(posicion){
+/*function livelyBar(posicion){
 	return function(){
 		lively_bar[posicion].classList.add('active');
 		row_input_content[posicion].classList.add('active');
 		for(var b=0; b<input_container.length; b++){
 			input_value = form_input[posicion].value;
-			/*row_input_content[b].classList.remove('active');
-			lively_bar[b].classList.remove('active');*/
 			if(input_value === ''){
 				console.log('Todos los  input estan vacio');
 			}else{
@@ -23,17 +21,34 @@ function livelyBar(posicion){
 
 for(var a=0; a<input_container.length; a++){
 	input_container[a].addEventListener('click', livelyBar(a));
-}
+}*/
 
 //Script para el select
-var form_select = document.querySelector('.form-select'),
+/*var form_select = document.querySelector('.form-select'),
 	ul_form_select = document.querySelector('.ul-form-select');
 
 form_select.addEventListener('click', function(){
 	ul_form_select.classList.add('active');
+});*/
+
+//Para que si no escribes nada, regrese a aus estado
+$('.form-input').focusin(function() {
+	$(this).parent().find('.row-input-content').addClass('active');
+
 });
-
-
+$('.form-input').focusout(function() {
+	$(this).parent().find('.row-input-content').removeClass('active');
+});
+$('body').on('change', '.form-input', function(event) {
+	$input = $(this);
+	if ($input.val().length < 1) {
+		$(this).parent().find('.row-input-content').removeClass('active1');
+		console.log('Estoy vacio');
+	}else{
+		$(this).parent().find('.row-input-content').addClass('active1');
+		console.log('Estoy lleno');
+	}
+});
 
 
 
